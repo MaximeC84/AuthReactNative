2f2f7c587d6cb84c8c2516818db30e6576f095d0
Maxime <maxime.chapot97@gmail.com>
2023-07-14 18:11:15 +0200

    Finalizing to the authentication and setup a firebase backend

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,33 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useContext, useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "./../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
-  return <AuthContent />;
+	const [isAuthenticating, setIsAuthenticating] = useState(false);
+	const authContext = useContext(AuthContext);
+	const signupHandler = async ({ email, password }) => {
+		setIsAuthenticating(true);
+
+		try {
+			const token = await createUser(email, password);
+			authContext.authenticate(token);
+		} catch (error) {
+			Alert.alert(
+				"Authentication failed.",
+				"Could not create user, please check your inputs and try again later."
+			);
+			setIsAuthenticating(false);
+		}
+	};
+
+	if (isAuthenticating) {
+		return <LoadingOverlay message="Creating user..." />;
+	}
+
+	return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
